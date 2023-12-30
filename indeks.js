1b8d9fcb25e9f9b79833bde3705f744015ac0baa
Aida Zametica <azametica1@etf.unsa.ba>
2023-12-30 21:21:04 +0100

    More routes done for first task

--- indeks.js
+++ indeks.js
@@ -16,25 +16,49 @@ app.use(
         })
 );
 
-// Definisanje ruta za dinamički prikaz stranica
-app.get('/:page', (req, res) => {
-    const page = req.params.page;
-    res.sendFile(path.join(__dirname, 'public', 'html', `${page}`));
-});
+// Hesiranje
+async function hashPassword(plainTextPassword) {
+    return await bcrypt.hash(plainTextPassword, 10);
+}
 
+// Pomocne funkcije za rute
 function findKorisnik(username, password, callback) {
-    fs.readFile(path.join(__dirname, 'data', 'korisnici.json'), 'utf8', (error, data) => {
+    fs.readFile(path.join(__dirname, 'data', 'korisnici.json'), 'utf8', async (error, data) => {
         if (error) {
             console.log(error);
             callback(error, null);
             return;
         }
         const users = JSON.parse(data);
-        const user = users.find(u => u.username === username && u.password === password);
-        callback(null, user);
+        const user = users.find(u => u.username === username);
+
+        const validPassword = await bcrypt.compare(password, user.password);
+        if (!validPassword) {
+            console.log(error);
+            callback(error, null);
+            return;
+        }
+        else{
+            callback(null, user);
+        }
+        
     });
 }
 
+function findKorisnikByUsername(username, callback) {
+    fs.readFile(path.join(__dirname, 'data', 'korisnici.json'), 'utf8', async (error, data) => {
+        if (error) {
+            console.log(error);
+            callback(error, null);
+            return;
+        }
+        const users = JSON.parse(data);
+        const user = users.find(u => u.username === username);
+        callback(null, user);  
+    });
+}
+
+// Dodatne rute
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
 
@@ -46,6 +70,78 @@ app.post('/login', (req, res) => {
             res.status(200).json({ poruka: 'Uspješna prijava' });
         }
     });
+});
+
+app.post('/logout', (req, res) => {
+     if (req.session.username) {
+         req.session.username = null;
+        res.status(200).json({ poruka: 'Uspješno ste se odjavili' });           
+    } else {
+        res.status(401).json({ greska: 'Neautorizovan pristup' });
+    }
+
+});
+
+app.get('/korisnik', (req, res) => {
+    if (req.session.username) {
+        findKorisnikByUsername(req.session.username, (error, user) => {
+            if (error || !user) {
+                res.status(401).json({ greska: 'Neautorizovan pristup' });
+            } else {
+                res.status(200).json({ id:user.id, ime:user.ime, prezime:user.prezime,username:user.username,password:user.password });
+            }
+        });     
+   } else {
+       res.status(401).json({ greska: 'Neautorizovan pristup' });
+   }
+
+});
+
+// TODO : refactor uraditi callback funkcija u promises
+app.put('/korisnik', async(req, res) => {
+    if (req.session.username) {
+
+        const data = await fs.promises.readFile(path.join(__dirname, 'data', 'korisnici.json'), 'utf8');
+        const users = JSON.parse(data);
+
+            await Promise.all(users.map(async user => {
+                if (user.username === req.session.username) {
+                    user.ime = req.body.ime || user.ime;
+                    user.prezime = req.body.prezime || user.prezime;
+                    user.username = req.body.username || user.username;
+                    if (req.body.password) {
+                        const hashedPassword = await hashPassword(req.body.password)
+                        user.password = hashedPassword;
+                    }
+                }
+                return user;
+            }));
+
+            await fs.promises.writeFile(path.join(__dirname, 'data', 'korisnici.json'), JSON.stringify(users));
+            res.status(200).json({ poruka: 'Podaci su uspješno ažurirani' });
+        
+    } else {
+        res.status(401).json({ greska: 'Neautorizovan pristup' });
+    }
+});
+
+app.get('/nekretnine', (req, res) => {
+    fs.readFile(path.join(__dirname, 'data', 'nekretnine.json'), 'utf8', async (error, data) => {
+        if (error) {
+            console.error(err);
+            res.status(500).json({ error: 'Ne čita dobro iz datoteke' });
+            return;
+        }
+            const nekretnine = JSON.parse(data);
+            res.status(200).json(nekretnine);
+        
+    });
+});
+
+// Definisanje ruta za dinamički prikaz stranica
+app.get('/:page', (req, res) => {
+    const page = req.params.page;
+    res.sendFile(path.join(__dirname, 'public', 'html', `${page}`));
 });
 
 const PORT = 3000;
