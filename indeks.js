87c9c2ba05ff1a5b1f352639c5a5aca4e1515bcc
Aida Zametica <azametica1@etf.unsa.ba>
2023-12-30 03:26:13 +0100

    Login route done

--- indeks.js
+++ indeks.js
@@ -1,14 +1,51 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 
 const app = express();
-
+app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    session({
+        secret:'NAJDNJKADBGRGSKSNA',
+        resave:false,
+        saveUninitialized:false
+        })
+);
 
-// GET requestovi sa nazivom stranice na portu 3000 kao odgovor dobiju HTML stranice iz foldera public/html
+// Definisanje ruta za dinamički prikaz stranica
 app.get('/:page', (req, res) => {
     const page = req.params.page;
     res.sendFile(path.join(__dirname, 'public', 'html', `${page}`));
+});
+
+function findKorisnik(username, password, callback) {
+    fs.readFile(path.join(__dirname, 'data', 'korisnici.json'), 'utf8', (error, data) => {
+        if (error) {
+            console.log(error);
+            callback(error, null);
+            return;
+        }
+        const users = JSON.parse(data);
+        const user = users.find(u => u.username === username && u.password === password);
+        callback(null, user);
+    });
+}
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    findKorisnik(username, password, (error, user) => {
+        if (error || !user) {
+            res.status(401).json({ greska: 'Neuspješna prijava' });
+        } else {
+            req.session.username = username;
+            res.status(200).json({ poruka: 'Uspješna prijava' });
+        }
+    });
 });
 
 const PORT = 3000;
