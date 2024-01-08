dc57b73bc4207f532be22d7ff8587d0d449e012b
Aida Zametica <azametica1@etf.unsa.ba>
2024-01-08 02:34:31 +0100

    all routes from the task one changed

--- indeks.js
+++ indeks.js
@@ -41,32 +41,6 @@ async function findKorisnik(username, password) {
     }
 }
 
-async function findKorisnikByUsername(username) {
-    try {
-        const user = await db.korisnik.findOne({where: { username: username },});
-        if (!user) {
-            return null;
-        }
-        return user;
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
-}
-
-async function findNekretninaById(nekretnina_id) {
-    try {
-        const nekretnina = await db.nekretnina.findByPk(nekretnina_id);
-        if (!nekretnina) {
-            return null;
-        }
-        return nekretnina;
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
-}
-
 // Dodatne rute
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
@@ -92,18 +66,13 @@ app.post('/logout', (req, res) => {
     } else {
         res.status(401).json({ greska: 'Neautorizovan pristup' });
     }
-
 });
 
 app.get('/korisnik', async (req, res) => {
     if (req.session.username) {
         try {
-            const user = await findKorisnikByUsername(req.session.username);
-            if (!user) {
-                res.status(401).json({ greska: 'Neautorizovan pristup' });
-            } else {
-                res.status(200).json({ id:user.id, ime:user.ime, prezime:user.prezime,username:user.username,password:user.password });
-            }
+            const user = await db.korisnik.findOne({where: { username: req.session.username }});
+            res.status(200).json({ id:user.id, ime:user.ime, prezime:user.prezime,username:user.username,password:user.password });
         } catch (error) {
             console.error(error);
             res.status(500).json({ greska: 'Problem sa nabavljanjem podataka iz baze' });
@@ -111,32 +80,26 @@ app.get('/korisnik', async (req, res) => {
    } else {
        res.status(401).json({ greska: 'Neautorizovan pristup' });
    }
-
 });
 
-// TODO : refactor uraditi callback funkcija u promises
 app.put('/korisnik', async(req, res) => {
     if (req.session.username) {
+        try {
+                const user = await db.korisnik.findOne({where: { username: req.session.username }});
+                user.ime = req.body.ime || user.ime;
+                user.prezime = req.body.prezime || user.prezime;
+                user.username = req.body.username || user.username;
 
-        const data = await fs.promises.readFile(path.join(__dirname, 'data', 'korisnici.json'), 'utf8');
-        const users = JSON.parse(data);
-
-            await Promise.all(users.map(async user => {
-                if (user.username === req.session.username) {
-                    user.ime = req.body.ime || user.ime;
-                    user.prezime = req.body.prezime || user.prezime;
-                    user.username = req.body.username || user.username;
-                    if (req.body.password) {
-                        const hashedPassword = await hashPassword(req.body.password)
-                        user.password = hashedPassword;
-                    }
+                if (req.body.password) {
+                    const hashedPassword = await hashPassword(req.body.password);
+                    user.password = hashedPassword;
                 }
-                return user;
-            }));
-
-            await fs.promises.writeFile(path.join(__dirname, 'data', 'korisnici.json'), JSON.stringify(users));
-            res.status(200).json({ poruka: 'Podaci su uspješno ažurirani' });
-        
+                await user.save();
+                res.status(200).json({ poruka: 'Podaci su uspješno ažurirani' });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ greska: 'Problem sa nabavljanjem korisnika iz baze' });
+        }
     } else {
         res.status(401).json({ greska: 'Neautorizovan pristup' });
     }
@@ -146,25 +109,24 @@ app.post('/upit', async(req, res) => {
     if (req.session.username) {
         const { nekretnina_id, tekst_upita } = req.body;
         try {
-            const user = await findKorisnikByUsername(req.session.username);
-                try{
-                    const nekretnina = await findNekretninaById(nekretnina_id);
-                    if (!nekretnina) {
-                        res.status(400).json({ greska: `Nekretnina sa id-em ${nekretnina_id} ne postoji`});
-                    } else {
-                        await db.upit.create({nekretnina_id: nekretnina.id,korisnik_id: user.id,tekst_upita: tekst_upita});
-                        res.status(200).json({ poruka: 'Upit je uspješno dodan'})
-                    }
+            const user = await db.korisnik.findOne({where: { username: req.session.username }});
+            try{
+                const nekretnina = await db.nekretnina.findByPk(nekretnina_id);
+                if (!nekretnina) {
+                     res.status(400).json({ greska: `Nekretnina sa id-em ${nekretnina_id} ne postoji`});
+                } else {
+                    await db.upit.create({nekretnina_id: nekretnina.id,korisnik_id: user.id,tekst_upita: tekst_upita});
+                    res.status(200).json({ poruka: 'Upit je uspješno dodan'})
                 }
-                catch (error) {
-                    console.error(error);
-                    res.status(500).json({ greska: 'Problem sa nabavljanjem nekretnine iz baze' });
-                }               
+            }
+            catch (error) {
+                console.error(error);
+                res.status(500).json({ greska: 'Problem sa nabavljanjem nekretnine iz baze' });
+            }               
         } catch (error) {
             console.error(error);
             res.status(500).json({ greska: 'Problem sa nabavljanjem korisnika iz baze' });
-        }
-        
+        }       
     } else {
         res.status(401).json({ greska: 'Neautorizovan pristup' });
     }
@@ -174,20 +136,15 @@ app.post('/upit', async(req, res) => {
 app.get('/korisnik', async (req, res) => {
     if (req.session.username) {
         try {
-            const user = await findKorisnikByUsername(req.session.username);
-            if (!user) {
-                res.status(401).json({ greska: 'Neautorizovan pristup' });
-            } else {
-                res.status(200).json({ id:user.id, ime:user.ime, prezime:user.prezime,username:user.username,password:user.password });
-            }
+            const user = await db.korisnik.findOne({where: { username: req.session.username }});
+            res.status(200).json({ id:user.id, ime:user.ime, prezime:user.prezime, username:user.username, password:user.password });
         } catch (error) {
             console.error(error);
             res.status(500).json({ greska: 'Problem sa nabavljanjem podataka iz baze' });
         }    
-   } else {
-       res.status(401).json({ greska: 'Neautorizovan pristup' });
-   }
-
+    } else {
+        res.status(401).json({ greska: 'Neautorizovan pristup' });
+    }
 });
 
 app.get('/nekretnine', async (req, res) => {
@@ -201,7 +158,6 @@ app.get('/nekretnine', async (req, res) => {
 });
 
 // Treci zadatak
-
 app.post('/marketing/nekretnine', async (req, res) => {
     const { nizNekretnina } = req.body;       
     const osvjezavanjaData = await fs.promises.readFile(path.join(__dirname, 'data', 'osvjezavanja.json'), 'utf8');
@@ -217,9 +173,7 @@ app.post('/marketing/nekretnine', async (req, res) => {
     });
 
     await fs.promises.writeFile(path.join(__dirname, 'data', 'osvjezavanja.json'), JSON.stringify(osvjezavanja));
-
     res.status(200).send();
-  
 });
 
 app.post("/marketing/nekretnina/:id", async (req, res) => {
@@ -247,18 +201,14 @@ app.post("/marketing/osvjezi", async (req, res) => {
   
     console.log("Sad je poslan samo 1",nizNekretnina)
       
-    const osvjezavanjaData = await fs.promises.readFile(path.join(__dirname, "data", "osvjezavanja.json"),"utf8");
-      
+    const osvjezavanjaData = await fs.promises.readFile(path.join(__dirname, "data", "osvjezavanja.json"),"utf8"); 
     const osvjezavanja = osvjezavanjaData ? JSON.parse(osvjezavanjaData) : [];
-
     const noviPodaci = osvjezavanja.filter((u) =>nizNekretnina.find((i) => i === parseInt(u.id)));
 
     console.log("novi podaci su",noviPodaci);
 
     res.status(200).json({ nizNekretnina: noviPodaci }); 
-
-  });
-
+});
 
 app.post("/hesiranje",async(req,res) =>{
     const { username,password} = req.body;
